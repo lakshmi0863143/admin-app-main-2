56f3afefa0611525a4e4fb1c2e78856bf3ff6a3f
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-11-14 10:44:24 +0000

    Edited log_in_form.Form1

--- client_code/log_in_form/Form1/__init__.py
+++ client_code/log_in_form/Form1/__init__.py
@@ -11,3 +11,22 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+if self.text_box_1.text == "" or self.text_box_2.text == "" or  self.text_box_3.text == "" or not self.date_picker_1.date or not self.drop_down_1.selected_value or not self.drop_down_2.selected_value:
+      Notification("Fill All Required Details").show()
+    else:
+      data = tables.app_tables.admin_teams.search()
+      a = -1
+      for row in data:
+        a += 1
+
+      if a == -1:
+        alert("No Data Available Here")
+      else:
+        data[a]['name'] = self.text_box_1.text
+        data[a]['email'] = self.text_box_2.text
+        data[a]['number'] = self.text_box_3.text
+        data[a]['joining_date'] = self.date_picker_1.date
+        data[a]['role'] = self.drop_down_1.selected_value
+        data[a]['permission'] = self.drop_down_2.selected_value
+        
+        print(a)
