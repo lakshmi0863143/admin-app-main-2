955d3350ebf8fd380d3d38d460e660bc762635eb
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-11-28 08:00:15 +0000

    Edited Log_in_Home.user_issues.feedback_form

--- client_code/Log_in_Home/user_issues/feedback_form/__init__.py
+++ client_code/Log_in_Home/user_issues/feedback_form/__init__.py
@@ -22,5 +22,27 @@ class feedback_form(feedback_formTemplate):
     user_discription = self.text_area_1.text
     image = self.file_loader_1.file
     feedback_form = self.text_area_2.text
+
+  
+
+  def drop_down_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    if self.drop_down_1.selected_value == 'Account Inquiries':
+            # Add items to the dropdown dynamically
+      items_to_add = [ 'Balance Inquiries', 'Transaction History' ]
+      self.drop_down_2.items = items_to_add
+    elif self.drop_down_1.selected_value == 'Loan and Mortgage Inquiries':
+            # Add items to the dropdown dynamically
+      items_to_add = ['Loan Application Status ', 'Mortgage Payment Inquiries']
+      self.drop_down_2.items = items_to_add  
+    elif self.drop_down_1.selected_value == 'Account Management':
+            # Add items to the dropdown dynamically
+      items_to_add = ['Account Closure','Change of Contact Information']
+      self.drop_down_2.items = items_to_add  
+    elif self.drop_down_1.selected_value == 'Financial Planning and Advice':
+            # Add items to the dropdown dynamically
+      items_to_add = ['Inquiries About Investment Options', 'Retirement Planning Assistance']
+      self.drop_down_2.items = items_to_add  
+
     
     
